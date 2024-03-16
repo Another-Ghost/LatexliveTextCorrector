d14e7de3aeb17f19caad21a7cafb31c9bb2c7a64
Bo Li <lb284542@gmail.com>
2024-03-16 16:02:20 +0800

    string process

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Hello World');
+document.getElementById('button').addEventListener('click', function(){
+let text_input = document.getElementById('text1');
+let text_output = document.getElementById('text2');
+text_output.value = text_input.value.replace(/(\\[a-zA-Z]+\{[^}]*\})/g, '\$$1\$');
+text_input.textContent;
+});
