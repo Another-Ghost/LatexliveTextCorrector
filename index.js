19f42e673c16155592563876e8b6b540796888b9
Bo Li <lb284542@gmail.com>
2024-03-30 23:14:21 +0800

    处理多种特殊情况，如换行分隔，单字符清况，括号

--- index.js
+++ index.js
@@ -27,16 +27,17 @@ function convertFormulasToLaTeX(inStr, wordsToRemove = '') {
     inStr = inStr.replace(wordsToRemove, '');
     inStr = inStr.replace(/ +/g, ' '); //将多个空格替换为一个空格
     inStr = inStr.replace(/\n+/g, '\n'); //去除重复换行符
+    inStr = inStr.replace('输人', '输入'); 
     let str = inStr.trim(); //删除字符串两端的空白字符
     //console.log(inStr);
     let outStr = "";
     let equation = "";
     let bEquation = false;
-    let lastCharPos = 0;
 
-    let PushEquationToOutStr = () => {
-        if(/[\-<=>\\\^_{\|}]/.test(equation)){
-            outStr += ToLatex(equation, lastCharPos);
+    //处理并存储已确定的一个公式
+    let PushEquationToOutStr = (nextChar) => {
+        if(/[\-<=>\\\^_{\|}\/\*\+\(]/.test(equation) || /^[a-zA-Z0-9]$/.test(equation.trim())){
+            outStr += ToMarkdown(equation, nextChar);
         }
         else
         {
@@ -44,50 +45,53 @@ function convertFormulasToLaTeX(inStr, wordsToRemove = '') {
         }
         bEquation = false;
         equation = "";
-        lastCharPos = 0;
     }
 
+    function ToMarkdown(equation, nextChar)
+    {
+        equationSymbol = "$";
+        let prevChar = outStr[outStr.length-1];
+        if((!nextChar || nextChar.match(/[\n\r]/)) && (!prevChar || prevChar.match(/[\n\r]/)))
+        {
+            equationSymbol = "$$";
+        }
+        if(equation[equation.length-1] != '$' || equation[0] != '$$')
+        {
+            equation = insertCharAt(equation, equationSymbol, findLastNonWhitespaceChar(equation)+1); //在公式字符串的最后一个非空格字符的位置的后一个位置插入"$$")
+        }
+        if(equation[0] != '$' || equation[0] != '$$')
+        {
+            equation = equationSymbol + equation;
+        }
+        return equation;
+    }
     for(let i = 0; i < str.length; i++) {
         let c = str[i];
-        if(!bEquation)
-        {
+        //let nextChar = i < str.length - 1 ? str[i + 1] : '';
+        if(!bEquation){
             if(c.match(/[!-~]/)) { //判断是否是非空格ASCII字符
-                if(!bEquation) {
-                    if(!c.match(/[:,.]/)) //把开头的 ":" 排除在 $$ 外；因为之后一般会跟着换行符，所以没有写在 ToLatex 函数里
-                    {
-                        bEquation = true;
-                        equation += c;
-                        lastCharPos = equation.length; //记录最后一个字符的位置的后一个位置
-                    }
-                    else
-                    {
-                        outStr += c;
-                    }
-                }
-            } 
-            else
-            {
-                outStr += c;
+                if(!bEquation && !c.match(/[:,.]/)) //把开头的 ":" 排除在 $$ 外；因为之后一般会跟着换行符，所以没有写在 ToLatex 函数里
+                {
+                    bEquation = true;
+                } 
             }
         }
-        else
-        {
-            equation += c;
-            lastCharPos = equation.length; //记录最后一个字符的位置的后一个位置
-            if(c.match(/[\n\r]/))
+        else{
+            if((c.match(/[\n\r]/) && (!/\\begin/.test(equation) || /\\end/.test(equation))) || (!c.match(/[ -~]/) && !c.match(/[\n\r]/)))
             {
-                PushEquationToOutStr();
+                PushEquationToOutStr(c);
             }
-            else if (c.match(/[\u4e00-\u9fa5\u3000-\u303f\uff00-\uffef]/)) { //判断是否是中文字符
-                if (bEquation) {
-                    PushEquationToOutStr();
-                }
-                outStr += c;
-            }
+        }
+
+        if(bEquation){
+            equation += c;
+        }
+        else{
+            outStr += c;
         }
     }
     if(equation.length > 0) {
-        PushEquationToOutStr();
+        PushEquationToOutStr('');
     }
     console.log(outStr);
     return outStr;
@@ -105,16 +109,9 @@ function convertFormulasToLaTeX(inStr, wordsToRemove = '') {
         return `${firstPart}${charToInsert}${secondPart}`;
     }
 
-    function ToLatex(equation, lastCharPos)
-    {
-        if(equation[equation.length-1] != '$')
-        {
-            equation = insertCharAt(equation, "$$", lastCharPos); //在公式字符串的最后一个非空格字符的位置的后一个位置插入"$$")
-        }
-        if(equation[0] != '$')
-        {
-            equation = "$$" + equation;
-        }
-        return equation;
+    function findLastNonWhitespaceChar(str) {
+        const match = str.match(/(\S)\s*$/);
+        return match ? str.lastIndexOf(match[1]) : -1;
     }
+    
 }
