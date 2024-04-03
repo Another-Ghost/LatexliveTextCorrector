4296481729d0b3cc8d8a17cb93575a200c3a1bf2
Bo Li <lb284542@gmail.com>
2024-04-04 00:37:18 +0800

    blocks completed

--- index.js
+++ index.js
@@ -16,33 +16,30 @@ function convertFormulasToLaTeX(inStr, wordsToRemove = '') {
     {
         inStr = inStr.replace(/\\begin{array}{[^{}]*}/g, '\\begin{aligned}');
         inStr = inStr.replace(/\\end{array}/g, '\\end{aligned}');
+        inStr = inStr.replace(wordsToRemove, '');
     }
-    inStr = inStr.replace(wordsToRemove, '');
     inStr = inStr.replace(/ */g, ' '); //将多个空格替换为一个空格
     inStr = inStr.replace(/\n+/g, '\n'); //去除重复换行符
     inStr = inStr.replace(/输人/g, "输入");
     inStr = inStr.replace(/存人/g, "存入");
-    inStr = inStr.trim(); 
-
+    //inStr = inStr.trim(); 
     
-    BeginEndRegex = /\\begin\{(.*?)\}(.*?)\\end\{\1\}/;
-    ChineseRegex = /[\u4E00-\u9FA5\u3000-\u303F\uff00-\uffef]+/g;
-    wordRegex = /\b[a-zA-Z]{2,}\b/g; ///\b[a-zA-Z]{2,}\b/g
-    //let temp = inStr.match(/\\begin{(.*?)}([\s\S]*?)\\end{\1}/);
+    //BeginEndRegex = /\\begin\{(.*?)\}(.*?)\\end\{\1\}/;
+    //ChineseRegex = /[\u4E00-\u9FA5\u3000-\u303F\uff00-\uffef]+/g;
+    //wordRegex = /\b[a-zA-Z]{2,}\b/g; ///\b[a-zA-Z]{2,}\b/g
 
     let outStr = ""; //最终输出的字符串
+
     let blocks = SplitToBlocks(inStr);
 
-
-
     for(let i = 0; i < blocks.length; i++){
-        if(!blocks[i].match(BeginEndRegex)){
+        if(!blocks[i].match(/\\begin\{(.*?)\}(.*?)\\end\{\1\}/)){ //判断是否多行非全公式块，是则不需做任何处理
 
             let tempMap = {};
             let index = 0;
         
             // 替换 $\text{...}$ 结构
-            let processedBlock = blocks[i].replace(/\$\\text ？\{[^{}]*\}\$/g, match => {
+            let processedBlock = blocks[i].replace(/\$\\text ?\{[^{}]*\}\$/g, match => {
                 let placeholder = `__PLACEHOLDER${index++}__`;
                 tempMap[placeholder] = match;
                 return placeholder;
@@ -50,26 +47,22 @@ function convertFormulasToLaTeX(inStr, wordsToRemove = '') {
         
             let parts = processedBlock.split(/([\u4E00-\u9FA5\u3000-\u303F\uff00-\uffef]+)|( +[a-zA-Z]{2,} +)/).filter(part => part !== undefined);
             if(parts.length > 1){
-                processedBlock = processedBlock.replace(/, +?/g, '，');
-                processedBlock = processedBlock.replace(/: +?/g, '：');
+                block[i] = blocks[i].replace(/\\text ?\{([^{}]*)\}/g, '$1'); //非全公式块，去掉\text{}
+                //非公式行，替换中文句尾标点
+                block[i] = block[i].replace(/, +?/g, '，');
+                block[i] = block[i].replace(/: +?/g, '：');
 
-                processedBlock = processedBlock.replace(/[^\u4E00-\u9FA5\u3000-\u303F\uff00-\uffef]+/g, match => ` $` + match.trim() + '$ ' );
+                // 在非中文字符串前后加上$
+                block[i] = block[i].replace(/[^\u4E00-\u9FA5\u3000-\u303F\uff00-\uffef]+/g, match => ` $` + match.trim() + '$ ' );
 
-                processedBlock = processedBlock.replace(/\$([a-zA-Z]{2,})\$/g, '$1');
+                // 把英文单词前后的$去掉
+                block[i] = block[i].replace(/\$([a-zA-Z]{2,})\$/g, '$1');
                 
-                //TODO 非单行情况直接删除
-                blocks[i] = processedBlock.replace(/__PLACEHOLDER\d+__/g, placeholder => tempMap[placeholder]);
-                // for(let j = 0; j < parts.length; j++){
-                //     if(!parts[j].match(ChineseRegex) && !parts[j].match(wordRegex)){
-                //         parts[j] = AddToStartEnd(parts[j], "$");
-                //     }
-                //     blocks[i] += parts[j];
-                // }
+                //blocks[i] = processedBlock.replace(/__PLACEHOLDER\d+__/g, placeholder => tempMap[placeholder]);
             
             }
-            else
-            {
-                blocks[i] = AddToStartEnd(blocks[i], "$$");
+            else { //单行全公式块，只需整体前后加上$$
+                blocks[i] = AddToStartEnd(blocks[i], "$$"); 
             }
             
         }
@@ -82,13 +75,16 @@ function convertFormulasToLaTeX(inStr, wordsToRemove = '') {
         return toAdd+str+toAdd;
     }
 
+    // 将字符串分割为块
     function SplitToBlocks(str)
     {
-        let splits = str.split(/[\n\r]/g).filter(part => part !== undefined);
+        //先按换行分割
+        let splits = str.split(/[\n\r]/g).filter(part => part !== undefined); 
         let i = 0;
         let blocks = [];
         while(i < splits.length)
         {
+            //将\begin{x} ... \end{x} 视为一个块，所以需要合并行
             if(splits[i].match(/\\begin/))
             {
                 let j = i + 1;
@@ -112,6 +108,5 @@ function convertFormulasToLaTeX(inStr, wordsToRemove = '') {
         }
         return blocks;
     }
-
 
 }
